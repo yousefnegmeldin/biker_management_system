a31ccfdfa34288ffe93db43b6b6f8ac7c3b68fc5
yousefnegmeldin <yousefnegm@gmail.com>
2024-08-14 14:12:31 +0300

    initial commit

---
+++ src/main/java/com/brightskies/biker_system/BikerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.brightskies.biker_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerSystemApplication.class, args);
+	}
+
+}
