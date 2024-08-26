c39b181fc14d5294168be70cdcd840aaec719546
yousefnegmeldin <yousefnegm@gmail.com>
2024-08-26 14:05:01 +0300

    fixed security utils

--- src/main/java/com/brightskies/biker_system/authentication/utility/SecurityUtils.java
+++ src/main/java/com/brightskies/biker_system/authentication/utility/SecurityUtils.java
@@ -1,13 +1,26 @@
 package com.brightskies.biker_system.authentication.utility;
 
 import com.brightskies.biker_system.generalmodels.User;
+import com.brightskies.biker_system.generalrepositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SecurityUtils {
+    UserRepository userRepository;
+    @Autowired
+    public SecurityUtils(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 
     public static Long getCurrentUserId() {
         User userDetails = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(userDetails == null){
+            throw new RuntimeException("User not found");
+        }
 //        return Long.parseLong(userDetails.getUsername());
         return userDetails.getId();
     }
