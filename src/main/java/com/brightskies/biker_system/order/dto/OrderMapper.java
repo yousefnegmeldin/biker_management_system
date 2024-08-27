ab19635a082cd96094a3f3f6d93e96cbb87e7eea
NoorAlHadidi <noor.ahmed.alhadidi@gmail.com>
2024-08-27 13:37:43 +0300

    committing changes

--- src/main/java/com/brightskies/biker_system/order/dto/OrderMapper.java
+++ src/main/java/com/brightskies/biker_system/order/dto/OrderMapper.java
@@ -4,6 +4,9 @@ import com.brightskies.biker_system.order.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class OrderMapper {
 
@@ -24,16 +27,28 @@ public class OrderMapper {
                 );
     }
 
-//    public Order mapToOrder (OrderDto orderdto) {
-//        return new Order(
-//                (long) -1,
-//                orderdto.orderDate(),
-//                orderdto.address(),
-//                orderdto.amount(),
-//                orderdto.paymentMethod(),
-//                null,
-//                customerRepository.findById(orderdto.customerId()).orElseThrow(() -> new RuntimeException("Customer does not exist"))
-//        );
-//    }
+    /*public Order mapToOrder (OrderDto orderdto) {
+        return new Order(
+                (long) -1,
+                orderdto.orderDate(),
+                orderdto.address(),
+                orderdto.amount(),
+                orderdto.paymentMethod(),
+                null,
+                customerRepository.findById(orderdto.customerId()).orElseThrow(() -> new RuntimeException("Customer does not exist"))
+        );
+    }*/
+
+    public static List<OrderDto> toDTOList(List<Order> orders) {
+        return orders.stream()
+                .map(order -> new OrderDto(
+                        order.getOrderDate(),
+                        order.getAddress(),
+                        order.getAmount(),
+                        order.getPaymentMethod(),
+                        order.getCustomer().getId() // assuming Address has a label field
+                ))
+                .collect(Collectors.toList());
+    }
 
 }
