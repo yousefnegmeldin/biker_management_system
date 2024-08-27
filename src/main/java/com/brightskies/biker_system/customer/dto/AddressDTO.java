4afd19b578b4e73434bb3c3f516e74217edc9232
NoorAlHadidi <noor.ahmed.alhadidi@gmail.com>
2024-08-27 12:57:13 +0300

    adjusted address model

--- src/main/java/com/brightskies/biker_system/customer/dto/AddressDTO.java
+++ src/main/java/com/brightskies/biker_system/customer/dto/AddressDTO.java
@@ -1,4 +1,6 @@
 package com.brightskies.biker_system.customer.dto;
 
-public record AddressDTO(String label, String city, String street, String apartment){
+import com.brightskies.biker_system.general.enums.Zone;
+
+public record AddressDTO(String label, Zone zone, String city, String street, String apartment){
 }
