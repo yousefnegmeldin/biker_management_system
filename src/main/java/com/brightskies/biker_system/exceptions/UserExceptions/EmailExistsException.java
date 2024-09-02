faae0078afacc31979b865b0889b2199ca5fd3a7
yousefnegmeldin <yousefnegm@gmail.com>
2024-09-02 11:28:15 +0300

    removed redundant exception

--- src/main/java/com/brightskies/biker_system/exceptions/UserExceptions/EmailExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.brightskies.biker_system.exceptions.UserExceptions;
-
-public class EmailExistsException extends Exception{
-    public EmailExistsException(){
-        super("Email Already signed up in application");
-    }
-}
